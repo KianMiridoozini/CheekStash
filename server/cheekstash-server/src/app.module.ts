e394944a93c5c41fb9f16a674adac3c3eaf70fe3
KianMiridoozini <114469799+KianMiridoozini@users.noreply.github.com>
2025-02-14 02:00:56 +0100

    Initial setup:
    Added base scaffold for Server and Client
    
    Server will utilize NestJS
    Client will utilize Angular

---
+++ server/cheekstash-server/src/app.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { CollectionsModule } from './collections/collections.module';
+import { ReviewsModule } from './reviews/reviews.module';
+import { AuthModule } from './auth/auth.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),  // Loads .env file
+    MongooseModule.forRoot(process.env.MONGO_URI as string),
+    UsersModule, 
+    CollectionsModule, 
+    ReviewsModule, 
+    AuthModule, 
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ server/cheekstash-server/src/main.ts
@@ -0,0 +1,30 @@
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  // global validation pipes
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  // Swagger Configuration
+  const config = new DocumentBuilder()
+    .setTitle('CheekStash API')
+    .setDescription('API documentation for CheekStash platform')
+    .setVersion('1.0')
+    .addBearerAuth() // JWT token usage
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
+
+  await app.listen(3000);
+}
+bootstrap();
